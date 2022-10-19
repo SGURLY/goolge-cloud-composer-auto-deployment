1115fa4a32af4b4c53b1ae4067d5c3a24817a612
Moritz Stoll <moritz@quickcommerce.org>
2022-10-19 20:31:55 +0200

    add base64 back + required parameter

--- main.rb
+++ main.rb
@@ -1,7 +1,7 @@
 require "base64"
 
 bucket_name = *ENV["INPUT_BUCKET"]
-File.write "/GOOGLE_CREDENTIALS.json", *ENV["INPUT_SECRETS"].fetch
+File.write "/GOOGLE_CREDENTIALS.json", Base64.decode64(*ENV["INPUT_SECRETS"])
 `gcloud auth activate-service-account --key-file=/GOOGLE_CREDENTIALS.json`
 `rm /GOOGLE_CREDENTIALS.json`
 `gsutil rsync -d -c -R dags/ #{bucket_name}/dags`
