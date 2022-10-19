59ca2027d73568d445bec7907562b4f914a996b4
Moritz Stoll <moritz@quickcommerce.org>
2022-10-19 20:25:12 +0200

    Update main.rb

--- main.rb
+++ main.rb
@@ -1,6 +1,6 @@
 require "base64"
 
-bucket_name = *ENV["INPUT_BUCKET"].fetch
+bucket_name = *ENV["INPUT_BUCKET"]
 File.write "/GOOGLE_CREDENTIALS.json", *ENV["INPUT_SECRETS"].fetch
 `gcloud auth activate-service-account --key-file=/GOOGLE_CREDENTIALS.json`
 `rm /GOOGLE_CREDENTIALS.json`
